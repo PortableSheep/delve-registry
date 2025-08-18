2da663d95db87de6725be55acdf82c244c287455
Michael Gunderson <michael@portablesheep.com>
2025-08-17 21:17:34 -0500

    Remove plugins

--- json-linter-formatter/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/PortableSheep/json-linter-formatter
-
-go 1.24.2
-
-require github.com/PortableSheep/delve_sdk v0.0.0-20250813152836-4d1ab58ec7e0
-
-require github.com/gorilla/websocket v1.5.3 // indirect
--- sample_plugin/frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { defineCustomElement } from 'vue';
-import ChartPluginComponent from './PluginComponent.vue';
-
-// 1. Wrap the Vue component into a Custom Element constructor.
-const ChartPluginElement = defineCustomElement(ChartPluginComponent);
-
-// 2. Register the custom element with the browser.
-// The tag name MUST contain a hyphen.
-customElements.define('super-chart-plugin', ChartPluginElement);
